2f4f03947df36762c38c860dd24b8ff4e0508b84
devsoraia <53994316+devsoraia@users.noreply.github.com>
2025-02-10 18:19:58 +0000

    Update UserRepository.java

--- src/main/java/mindera/porto/AppMovie/repository/UserRepository.java
+++ src/main/java/mindera/porto/AppMovie/repository/UserRepository.java
@@ -3,5 +3,6 @@ package mindera.porto.AppMovie.repository;
 import mindera.porto.AppMovie.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
