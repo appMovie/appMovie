593d3903bbb3419853625da67119c4b18b2751f0
devsoraia <53994316+devsoraia@users.noreply.github.com>
2025-02-10 18:19:33 +0000

    Update UserController.java

--- src/main/java/mindera/porto/AppMovie/controller/UserController.java
+++ src/main/java/mindera/porto/AppMovie/controller/UserController.java
@@ -2,6 +2,7 @@ package mindera.porto.AppMovie.controller;
 
 import mindera.porto.AppMovie.model.User;
 import mindera.porto.AppMovie.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path= "/api/v1/user")
+@Tag(name = "User", description = "User management endpoints")
 
 public class UserController {
 
