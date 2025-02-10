f3a22168745fedb1648c780647d0a9b58b32df6f
devsoraia <53994316+devsoraia@users.noreply.github.com>
2025-02-10 18:20:54 +0000

    Update User.java

--- src/main/java/mindera/porto/AppMovie/model/User.java
+++ src/main/java/mindera/porto/AppMovie/model/User.java
@@ -2,68 +2,73 @@ package mindera.porto.AppMovie.model;
 
 import jakarta.persistence.*;
 
-@Entity
-@Table(name= "users")
-//outro teste
+@Entity(name= "users")
+@Table
+
+
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
-    private String userName;
+    @Column(unique = true, nullable = false)
+    private String username;
 
-    @Column(unique = false)
+    @Column(nullable = false)
     private String password;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
     @Enumerated(EnumType.STRING)
-    @Column(unique = false)
+
     private Role role;
 
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+
+    private List<Review> reviews;
+
     public User(){
-    }
-
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
     }
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+
+    public List<Review> getReviews(){ return reviews; }
+
+    public void setReviews(List<Review> reviews) { this.reviews = reviews;}
 }
