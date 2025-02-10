bfb94625f6b7e48d112df9c4e0dccf9c8816647a
devsoraia <53994316+devsoraia@users.noreply.github.com>
2025-02-10 18:18:56 +0000

    Update ReviewService.java

--- src/main/java/mindera/porto/AppMovie/service/ReviewService.java
+++ src/main/java/mindera/porto/AppMovie/service/ReviewService.java
@@ -1,4 +1,62 @@
-package mindera.porto.AppMovie.service;
+package mindera.porto.AppMovie.controller;
 
-public class ReviewService {
+import mindera.porto.AppMovie.model.Review;
+import mindera.porto.AppMovie.service.ReviewService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/v1/review")
+@Tag(name = "Review", description = "Review management endpoints")
+
+public class ReviewController {
+
+    @Autowired
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+
+    @GetMapping
+    public List<Review> getAllReviews() {
+        return reviewService.getAllReviews();
+    }
+
+    @PostMapping
+    public Review addReview(@RequestBody Review review) {
+        return reviewService.addReview(review);
+    }
+
+
+
+    @GetMapping("/{id}")
+    public Optional<Review> getReviewById(@PathVariable Long id) {
+        return reviewService.getReviewById(id);
+    }
+
+    @GetMapping("/user/{userId}")
+    public List<Review> getReviewsByUser(@PathVariable Long userId) {
+        return reviewService.getReviewsByUser(userId);
+    }
+
+    @GetMapping("/movie/{movieId}")
+    public List<Review> getReviewsByMovie(@PathVariable Long movieId) {
+        return reviewService.getReviewsByMovie(movieId);
+    }
+
+    @GetMapping("/tvshow/{tvShowId}")
+    public List<Review> getReviewsByTvShow(@PathVariable Long tvShowId) {
+        return reviewService.getReviewsByTvShow(tvShowId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReview(@PathVariable Long id) {
+        reviewService.deleteReview(id);
+    }
 }
